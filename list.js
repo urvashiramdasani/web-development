103b3be24a9219e4690f67c734b6ce0490b9402c
Urvashi Ramdasani <54858768+urvashiramdasani@users.noreply.github.com>
2020-02-28 00:54:07 +0530

    Update list.js

--- list.js
+++ list.js
@@ -3,11 +3,33 @@ var input = prompt("What would you like to do?");
 while(input!== "quit") {
 	var input = prompt("What would you like to do?");
 	if(input === "list") {
-		console.log(todos)
+		listTodos()
 	}
 	else if(input === "new") {
-		var newTodo = prompt("Enter new Todo : ");
-		todos.push(newTodo);
+		newTodos()
+	}
+	else if(input === "delete") {
+		deleteTodos()
 	}
 }
 console.log("OK, YOU QUIT THE APP");
+
+function listTodos() {
+	console.log("*******")
+	todos.forEach(function(todo,i) {
+		console.log(i+": "+todo)
+	})
+	console.log("*******")
+}
+
+function newTodos() {
+	var newTodo = prompt("Enter new Todo : ");
+	todos.push(newTodo);
+	console.log("Added Todo")
+}
+
+function deleteTodos() {
+	var index = prompt("Enter index of todo to delete : ")
+	todos.splice(index,1)
+	console.log("Deleted Todo")
+}
